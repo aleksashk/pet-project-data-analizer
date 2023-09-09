a4a05836981fdaf29d676ef3271d4a2dcf5c7e28
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-09 16:01:24 +0300

    refactoring: delete @ResponseStatus(HttpStatus.ACCEPTED  from AnalyzerController

--- src/main/java/by/aleksandrphilimonov/data/analyzer/controller/AnalyzerController.java
+++ src/main/java/by/aleksandrphilimonov/data/analyzer/controller/AnalyzerController.java
@@ -6,16 +6,13 @@ import by.aleksandrphilimonov.data.analyzer.service.AnalyzerService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
-@ResponseStatus(HttpStatus.ACCEPTED)
 @Data
 @RequiredArgsConstructor
 public class AnalyzerController {
