6fa1917e7a979f31f04d4ab654375f537de00102
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-09 15:38:51 +0300

    refactoring: add @NotNull

--- src/main/java/by/aleksandrphilimonov/data/analyzer/json/DataRequest.java
+++ src/main/java/by/aleksandrphilimonov/data/analyzer/json/DataRequest.java
@@ -1,12 +1,11 @@
 package by.aleksandrphilimonov.data.analyzer.json;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class DataRequest {
+    @NotNull
     private String text;
 }
